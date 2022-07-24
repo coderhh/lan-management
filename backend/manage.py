423d99023eea72207d9c757649093b56604f6ebc
coderhh <yehanghan@gmail.com>
2022-07-24 21:51:37 +0800

    add refresh button

--- backend/manage.py
+++ backend/manage.py
@@ -21,8 +21,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def run():
-    app.run(ssl_context=('conf/cert.pem', 'conf/key.pem'))
-
+    app.run(host='0.0.0.0')
 
 @manager.command
 def test():
