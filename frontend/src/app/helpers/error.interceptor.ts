e8f82a5dede9f227ed1d2c4c87eda59b06ed3be0
coderhh <yehanghan@gmail.com>
2022-07-31 15:47:35 +0800

    fix lint error interceptor

--- frontend/src/app/helpers/error.interceptor.ts
+++ frontend/src/app/helpers/error.interceptor.ts
@@ -6,15 +6,15 @@ import { AccountService } from '../service/account.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(
-      private accountService: AccountService){}
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(req).pipe(catchError(err => {
-            if([401, 403].includes(err.status) && this.accountService.accountValue){
-                this.accountService.logout();
-            }
-            const error = (err && err.error && err.error.message) || err.statusText;
-            return throwError(error);
-        }))
-    }
+  constructor (
+    private accountService: AccountService) { }
+  intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(req).pipe(catchError(err => {
+      if ([401, 403].includes(err.status) && this.accountService.accountValue) {
+        this.accountService.logout();
+      }
+      const error = (err && err.error && err.error.message) || err.statusText;
+      return throwError(error);
+    }))
+  }
 }
