389d83951c3fe05fe8c960cd1ba7267063290b98
coderhh <yehanghan@gmail.com>
2022-08-19 10:58:35 +0800

    update account service add unit test

--- frontend/src/app/service/account.service.spec.ts
+++ frontend/src/app/service/account.service.spec.ts
@@ -1,21 +1,52 @@
-import { TestBed, inject } from '@angular/core/testing';
 import {
   HttpClientTestingModule,
   HttpTestingController
 } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Account } from '../models/account';
 import { AccountService } from './account.service';
+import { environment } from '../../environments/environment';
+
+const baseUrl = `${environment.apiUrl}/account/`;
 
 describe('AccountService', () => {
-  let httpTestingController: HttpTestingController;
-  let accountService: AccountService;
-
+  let service: AccountService;
+  let httpMock: HttpTestingController;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [AccountService]
     });
-    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.inject(AccountService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
-  beforeEach(inject([AccountService], (service: AccountService) => {
-    accountService = service;
-  }));
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('be able to retrieve accounts from API bia GET', () => {
+    const dummyAccounts: Account[] = [
+      {
+        public_id: '1',
+        last_name: 'last_name'
+      },
+      {
+        public_id: '2',
+        last_name: 'last_name'
+      }
+    ];
+    service.getAll().subscribe((accounts) => {
+      expect(accounts.length).toBe(2);
+      expect(accounts).toEqual(dummyAccounts);
+    });
+    const request = httpMock.expectOne(baseUrl);
+    expect(request.request.method).toBe('GET');
+    request.flush(dummyAccounts);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
 });
